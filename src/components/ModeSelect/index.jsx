8dd7be18fdce35db1fb5fca64e27590dc4f53fb8
LongNguyen <longnguyen.dev225@gmail.com>
2023-11-18 21:47:54 +0700

    add note

--- src/components/ModeSelect/index.jsx
+++ src/components/ModeSelect/index.jsx
@@ -2,14 +2,17 @@ import FormControl from '@mui/material/FormControl'
 import InputLabel from '@mui/material/InputLabel'
 import MenuItem from '@mui/material/MenuItem'
 import Select from '@mui/material/Select'
-import { useColorScheme } from '@mui/material'
+import { Box, useColorScheme } from '@mui/material'
+import LightModeIcon from '@mui/icons-material/LightMode'
+import DarkModeIcon from '@mui/icons-material/DarkMode'
+import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
 
 const ModeSelect = () => {
   const { mode, setMode } = useColorScheme()
 
   return (
     <div>
-      <FormControl sx={{ m: 1, minWidth: 80 }}>
+      <FormControl sx={{ m: 1, minWidth: 120 }} size='small'>
         <InputLabel id='demo-simple-select-autowidth-label'>Mode</InputLabel>
         <Select
           labelId='demo-simple-select-autowidth-label'
@@ -19,9 +22,21 @@ const ModeSelect = () => {
           autoWidth
           label='Mode'
         >
-          <MenuItem value='light'>Light</MenuItem>
-          <MenuItem value='dark'>Dark</MenuItem>
-          <MenuItem value='system'>System</MenuItem>
+          <MenuItem value='light'>
+            <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+              <LightModeIcon fontSize='smaill' /> Light
+            </div>
+          </MenuItem>
+          <MenuItem value='dark'>
+            <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+              <DarkModeIcon fontSize='smaill' /> Dark
+            </Box>
+          </MenuItem>
+          <MenuItem value='system'>
+            <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+              <SettingsBrightnessIcon fontSize='smaill' /> System
+            </Box>
+          </MenuItem>
         </Select>
       </FormControl>
     </div>
